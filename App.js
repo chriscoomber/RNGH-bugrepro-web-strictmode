9056ea4a95711633641c5382098edac708eeb5fd
Chris Coomber <ccoomber@whiterosemaths.com>
2024-07-11 13:32:28 +0100

    Add draggable square and React.StrictMode

--- App.js
+++ App.js
@@ -1,12 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import Animated, { useSharedValue, useAnimatedStyle } from 'react-native-reanimated';
+import { GestureHandlerRootView, Gesture, GestureDetector } from 'react-native-gesture-handler';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <React.StrictMode>
+      <GestureHandlerRootView style={{flex: 1}}>
+        <View style={styles.container}>
+          <DragSquare/>
+        </View>
+      </GestureHandlerRootView>
+    </React.StrictMode>
+  );
+}
+
+function DragSquare() {
+  const x = useSharedValue(0);
+  const y = useSharedValue(0);
+
+  const style = useAnimatedStyle(
+    () => ({ left: x.value, top: y.value }),
+    [x, y]
+  );
+
+  const pan = Gesture.Pan().onUpdate((e) => {
+    x.value = e.translationX;
+    y.value = e.translationY;
+  });
+
+  return (
+    <GestureDetector gesture={pan}>
+      <Animated.View
+        style={[
+          {
+            height: 20,
+            width: 20,
+            backgroundColor: 'red',
+            borderRadius: 5,
+            position: 'absolute',
+          },
+          style,
+        ]}
+      />
+    </GestureDetector>
   );
 }
 
